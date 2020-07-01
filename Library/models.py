dca458f6c168764ef1961639f64f19d7f1377f85
Francois Mukaba <fxkikomina@gmail.com>
2020-07-01 15:31:14 -0700

    POST request done

--- Library/models.py
+++ Library/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-class Books(models.Model):
+class Book(models.Model):
     title = models.TextField()
     author = models.TextField()
     year = models.DateTimeField()
     pages = models.IntegerField()
-    description = models.CharField(300)
+    description = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __repr__(self):
         return f"{self.title} by {self.author}. Published in {self.year}"
--- Library/views.py
+++ Library/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Book
 
 # Home page
 def index(request):
@@ -6,7 +7,21 @@ def index(request):
 
 # Add a book
 def create(request):
-    return render(request, "create.html")
+    if request.method == "POST":
+        title = request.POST["title"]
+        author = request.POST["author"]
+        year = request.POST["year"]
+        pages = request.POST["pages"]
+        description = request.POST["description"]
+
+        new_book = Book(title=title, author=author, year=year, pages=pages, description=description)  
+        new_book.save()
+
+        return redirect("/list_all")
+    elif request.method == "GET":
+        return render(request, "create.html")
+    else:
+        return HttpResponse("Operation not allowed")
 
 # List all books 
 def list_all(request):
